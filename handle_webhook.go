74172e1413c60f7d8e8e0b088cf7a5b1ce1389e3
Clement Seconde-Kynnersely <c.sec.kyn@gmail.com>
2024-01-18 08:41:22 +0000

    feat(webhook): adding webhook call and updating datatabase function to handle IsChirpy property

--- handle_webhook.go
+++ handle_webhook.go
@@ -1,12 +1,42 @@
 package main
 
-import "net/http"
+import (
+	"encoding/json"
+	"errors"
+	"net/http"
 
-type WebhookPayload struct {
-	Event string `json:"event"`
-	Data  User   `json:"data"`
-}
+	"github.com/ClemSK/chirpy/internal/database"
+)
 
 func (cfg *apiConfig) handleWebhook(w http.ResponseWriter, r *http.Request) {
-	//
+	type parameters struct {
+		Event string `json:"event"`
+		Data  struct {
+			UserId int `json:"user_id"`
+		}
+	}
+
+	decoder := json.NewDecoder(r.Body)
+	params := parameters{}
+	err := decoder.Decode(&params)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Could not decode parameter")
+		return
+	}
+
+	if params.Event != "user.upgraded" {
+		respondWithJSON(w, http.StatusOK, struct{}{})
+		return
+	}
+
+	_, err = cfg.DB.UpgradeChirpyRed(params.Data.UserId)
+	if err != nil {
+		if errors.Is(err, database.ErrNotExist) {
+			respondWithError(w, http.StatusNotFound, "Could not find user")
+			return
+		}
+		respondWithError(w, http.StatusInternalServerError, "Could not update user")
+		return
+	}
+	respondWithJSON(w, http.StatusOK, struct{}{})
 }
