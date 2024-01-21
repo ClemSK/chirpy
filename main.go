10fd2d28c39df80a4da518015786cf789fae1f3d
Clement Seconde-Kynnersely <c.sec.kyn@gmail.com>
2024-01-21 14:10:08 +0000

    feat(webhooks): api key for accessing Chripy Red

--- main.go
+++ main.go
@@ -17,6 +17,7 @@ type apiConfig struct {
 	fileserverHits int
 	DB             *database.DB
 	jwtSecret      string
+	polkaKey       string
 }
 
 func main() {
@@ -28,6 +29,11 @@ func main() {
 	jwtSecret := os.Getenv("JWT_SECRET")
 	if jwtSecret == "" {
 		log.Fatal("JWT_SECRET environment variable not set")
+	}
+
+	polkaKey := os.Getenv("POLKA_KEY")
+	if polkaKey == "" {
+		log.Fatal("POLKA_KEY environment variable is not set")
 	}
 
 	db, err := database.NewDB("database.json")
@@ -51,6 +57,7 @@ func main() {
 		fileserverHits: 0,
 		DB:             db,
 		jwtSecret:      jwtSecret,
+		polkaKey:       polkaKey,
 	}
 
 	r := chi.NewRouter() // base router
